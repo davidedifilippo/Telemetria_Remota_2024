d662a6842cdfbc639a6b27ea90b58e85366304eb
Ing. Di Filippo <davidedifilippo@gmail.com>
2024-03-04 19:00:52 +0100

    Create secret.h

---
+++ mqtt_client/secret.h
@@ -0,0 +1,5 @@
+
+///////please enter your sensitive data in the Secret tab/arduino_secrets.h
+
+char ssid[] = "" ;    // your network SSID (name)
+char pass[] = "";    // your network password (use for WPA, or use as key for WEP)
